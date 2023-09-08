3e241f983766c0321b0df9aecdc8683e6a25d994
Teriri <1511736399@qq.com>
2023-09-08 21:56:13 +0800

    Initial commit

---
+++ src/test/java/com/example/test/dao/StudentDaoTest.java
@@ -0,0 +1,10 @@
+package com.example.test.dao;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class StudentDaoTest {
+
+}
