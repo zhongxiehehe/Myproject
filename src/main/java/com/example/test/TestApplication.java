710489088a00a6ba4ab92d09717390001acec5bc
怀念66 <2870843924@qq.com>
2023-09-08 22:51:41 +0800

    TestApplication添加一个测试方法：testFunction

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestApplication {
-
+    public void testFunction(){
+        System.out.println("这是一个测试方法！");
+    }
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
     }
