36e9ca88818eca54a1086ac19237477c70ba169a
Teriri <1511736399@qq.com>
2023-09-08 21:54:00 +0800

    Initial commit

--- src/main/java/com/example/test/dao/StudentDao.java
+++ src/main/java/com/example/test/dao/StudentDao.java
@@ -2,7 +2,9 @@ package com.example.test.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.test.model.Student;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StudentDao extends BaseMapper<Student> {
 
 }
