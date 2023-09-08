b4426f46c38cddc8a27c3e43800e19a1884e8ab3
怀念66 <2870843924@qq.com>
2023-09-08 22:52:49 +0800

    TestApplication添加二个测试方法：testFunction1，testFunction2

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -5,8 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestApplication {
-    public void testFunction(){
-        System.out.println("这是一个测试方法！");
+    public void testFunction1(){
+        System.out.println("这是一个测试方法1！");
+    }
+    public void testFunction2(){
+        System.out.println("这是一个测试方法2！");
     }
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
